7e4e82b932ef2964ddbe256cf1652be29c484462
elliottong15-arch <elliottong15@gmail.com>
2026-03-11 21:49:15 +0000

    Made a joke program

---
+++ jokes.py
@@ -0,0 +1,31 @@
+print("")
+print("")
+print("")
+name = input("\n---What's your name?\n")
+input()
+print(f"Hello {name}")
+input_1 = input("\n---Do you wanna hear some jokes? Type in yes if you want.---\n")
+
+if input_1 == "yes" or "Yes":
+    print("")
+    print("")
+    print("")
+    print("Why did the skeleton not cross the road?!!!")
+    input()
+    print("Because it didn't have the guts!!!")
+    input()
+    input_2 = input("\n---Do you wanna hear one more joke?---\n")
+    if input_2 == "yes" or "Yes":
+        print("")
+        print("")
+        print("")
+        print("Why did the chicken cross the road?")
+        input()
+        print("To get to the other side.")
+    elif input_2 == "no" or "No":
+        print("Ok cool")
+    else:
+        print("Ok cool")
+else:
+    print("Ok cool")
+    
--- welcome_to_ct8.py
+++ welcome_to_ct8.py
@@ -9,7 +9,8 @@ print("\n\n")
 print("This area is called the TERMINAL.")
 print("It shows messages and errors.")
 print("You also can type in the terminal.")
-name = input("\n---Type your name here, then press Enter---\n")
+name = input("\n---Type your first name here, then press Enter---\n")
+name_second = input("\n---Type your last name here, then press Enter---\n")
 
 print("\n\n")
 print(r"  _____ ____  __  __ _____    _______ _    _ _____ _   _ _  _______ _   _  _____    ___  ")
@@ -21,10 +22,22 @@ print(r" \_____\____/|_|  |_|_|         |_|  |_|  |_|_____|_| \_|_|\_\_____|_| \
 # text art from https://patorjk.com/software/taag/
 
 
-print(f"This codespace belongs to {name}")
+print(f"This codespace belongs to {name} {name_second}")
 print("\n\n")
-                                                                                          
+two_truths_and_one_lie = ["I play tennis", "I don't like sandwiches", "I broke 6 bones"]
+for two_truths_and_one_lie in two_truths_and_one_lie:
+    print(two_truths_and_one_lie)
+    lie = input("\n---Type the lie here---\n")
+    if lie == "I play tennis":
+        print("Sorry, that's wrong!")
+    elif lie == "I don't like sandwiches":
+        print("Sorry, that's wrong!")
+    elif lie == "I broke 6 bones":
+        print("Correct!! You Win!!!")
+    else:
+        print("I don't quite understand.")
+
                                                                                           
                                   
 print("Now it's your turn:")
-print("Find this line (line 30) in the welcome_to_ct8.py file, then change the message to say a fact about you.")                                  
+print("I play tennis often.")                                  
