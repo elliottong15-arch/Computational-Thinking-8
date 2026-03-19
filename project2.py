0a092fd289d228770a086169417a87236f50c6e5
elliottong15-arch <elliottong15@gmail.com>
2026-03-19 16:29:28 +0000

    Added other questions

--- project2.py
+++ project2.py
@@ -39,37 +39,116 @@ while True:
             print("Please type in A, a, B, b, C, or c...")
             continue
         while True:
-            answer3 = input("Last question. Which sport do you play and watch often? A) Tennis B) Basketball or C) Soccer?")
-            input()
+            answer3 = input("Which sport would you want to play in the future the most? A) Tennis B) Basketball C) Soccer or D) None?")
             if answer3 == "A" or answer3 == "a":
-                tennis_points += 1
+                tennis_points += 2
                 time.sleep(0.5)
                 print("Cool")
-                time.sleep(0.5)
-                print(f"Thank you for answering my questions {name}")
-                if tennis_points > basketball_points and tennis_points > soccer_points:
-                    print("I'm guessing that you're a tennis player")
-                    input()
             elif answer3 == "B" or answer3 == "b":
-                basketball_points += 1
-                time.sleep(0.5)
-                print("Cool")
-                time.sleep(0.5)
-                print(f"Thank you for answering my questions {name}")
-                if basketball_points > tennis_points and basketball_points > soccer_points:
-                    print("I'm guessing that you're a basketball player")
-                    input()
+                basketball_points += 2
             elif answer3 == "C" or answer3 == "c":
-                soccer_points += 1
-                time.sleep(0.5)
-                print("Cool")
-                time.sleep(0.5)
-                print(f"Thank you for answering my questions {name}")
-                if soccer_points > basketball_points and soccer_points > tennis_points:
-                    print("I'm guessing that you're a soccer player")
-                    input()
+                soccer_points += 2
+            elif answer3 == "D" or answer3 == "d":
+                tennis_points += 0
+                basketball_points += 0
+                soccer_points += 0
             else:
-                print("Sorry, I don't quite understand")
-                time.sleep(0.5)
-                print("Please type in A, a, B, b, C, or c...")
+                print("Sorry I do not understand. Please pick A, a, B, b, C, c, D, or d")
                 continue
+            while True:
+                answer4 = input("How many points do you have to play to for a set in a tennis game? A) 6 B) 7 or C) Depends")
+                if answer4 == "A" or answer4 == "a":
+                    soccer_points += 2
+                elif answer4 == "B" or answer4 == "b":
+                    basketball_points += 1
+                elif answer4 == "C" or answer4 == "c":
+                    tennis_points += 1
+                else:
+                    print("Sorry I do not understand. Please pick A, a, B, b, C, c, D, or d")
+                    continue   
+                while True:
+                    answer5 = input("Which sport do you play and watch often? A) Tennis B) Basketball or C) Soccer?")
+                    input()
+                    if answer5 == "A" or answer5 == "a":
+                        tennis_points += 1
+                        time.sleep(0.5)
+                        print("Cool")
+                        time.sleep(0.5)
+                        print(f"Thank you for answering my questions {name}")
+                        if tennis_points > basketball_points and tennis_points > soccer_points:
+                            print("I'm guessing that you like tennis")
+                            input()
+                            input()
+                            input()
+                            input()
+                            input()
+                        elif basketball_points > tennis_points and basketball_points > soccer_points:
+                            print("I'm guessing that you like basketball")
+                            input()
+                            input()
+                            input()
+                            input()
+                            input()
+                        elif soccer_points > basketball_points and soccer_points > tennis_points:
+                            print("I'm guessing that you like soccer")
+                            input()
+                            input()
+                            input()
+                            input()
+                            input()
+                    elif answer5 == "B" or answer5 == "b":
+                        basketball_points += 1
+                        time.sleep(0.5)
+                        print("Cool")
+                        time.sleep(0.5)
+                        print(f"Thank you for answering my questions {name}")
+                        if tennis_points > basketball_points and tennis_points > soccer_points:
+                            print("I'm guessing that you like tennis")
+                            input()
+                            input()
+                            input()
+                            input()
+                            input()
+                        elif basketball_points > tennis_points and basketball_points > soccer_points:
+                            print("I'm guessing that you like basketball")
+                            input()
+                            input()
+                            input()
+                            input()
+                            input()
+                        elif soccer_points > basketball_points and soccer_points > tennis_points:
+                            print("I'm guessing that you like soccer")
+                            input()
+                            input()
+                            input()
+                            input()
+                            input()
+                    elif answer5 == "C" or answer5 == "c":
+                        soccer_points += 1
+                        time.sleep(0.5)
+                        print("Cool")
+                        time.sleep(0.5)
+                        print(f"Thank you for answering my questions {name}")
+                        if tennis_points > basketball_points and tennis_points > soccer_points:
+                            print("I'm guessing that you like tennis")
+                            input()
+                            input()
+                            input()
+                            input()
+                        elif basketball_points > tennis_points and basketball_points > soccer_points:
+                            print("I'm guessing that you like basketball")
+                            input()
+                            input()
+                            input()
+                            input()
+                        elif soccer_points > basketball_points and soccer_points > tennis_points:
+                            print("I'm guessing that you like soccer")
+                            input()
+                            input()
+                            input()
+                            input()
+                    else:
+                        print("Sorry, I don't quite understand")
+                        time.sleep(0.5)
+                        print("Please type in A, a, B, b, C, or c...")
+                        continue
